5a11628f36eed16521b8be40d4fe69119f58ea10
Matheus Peixoto <matheusdaspeixoto@gmail.com>
2026-09-23 21:39:17 -0300

    Refactoring

--- src/main/java/org/stok/SocketServerMain.java
+++ src/main/java/org/stok/SocketServerMain.java
@@ -4,9 +4,7 @@ import org.stok.repository.ProductRepository;
 import org.stok.server.Server;
 import org.stok.service.StokService;
 
-import java.sql.SQLException;
-
-public class Main {
+public class SocketServerMain {
     public static void main(String[] args) {
         ProductRepository productRepository = new ProductRepository();
         StokService stokService = new StokService(productRepository);
